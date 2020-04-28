bac4a8fe2930c1666abb837323c49ba3fcf738c0
Seoyoung Kim <mintholic1@gmail.com>
2020-04-28 20:43:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  new_array = array.map do |words|
+  new_array = array.collect do |words|
     words.reverse
   end
     new_array.join(" ")
